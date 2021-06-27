cb9ea4ea178d5cc6ed5cf711bfe79e122071f700
ignaciocervino <ignaciocervino@gmail.com>
2021-06-27 17:57:24 -0300

    #07 Insurance calculations

--- js/app.js
+++ js/app.js
@@ -4,6 +4,47 @@ function Seguro(marca,year,tipo){
     this.year = year;
     this.tipo= tipo;
 }
+//Realizac la cotizacion con los datos
+Seguro.prototype.contizarSeguro = function(){
+    /**
+      1 = Americano 1.15
+      2 = Asiatico 1.05
+      3 = Europeo 1.35
+     */
+    let cantidad;
+    const base = 2000;
+    switch(this.marca){
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+        default:
+            cantidad = base;
+            break;
+    }
+
+    //Leer año
+    const diferencia = new Date().getFullYear() - this.year;
+
+    //Cada año que la diferencia es mayor, el costo va a reducirse un 3%
+    cantidad -= ((diferencia*3)*cantidad)/100;
+
+    //Si el seguro es basico se multiplica por un 30% mas
+    //Si el seguro es completo se multiplica por un 50% mas
+    if(this.tipo === 'basico'){
+        cantidad *= 1.30;
+    }
+    else{
+        cantidad *= 1.50;
+    }
+    return cantidad;
+}
+
 
 function UI(){}//Ojeto vacio para la interfaz
 
@@ -71,5 +112,10 @@ function contizarSeguro(e){
     }
     
     ui.mostrarMensaje('Corizando','exito');
+
+    //Instanciar el seguro
+    const seguro = new Seguro(marca,year,tipo);
+    //Utilizar el prototype que va a cotizar 
+    console.log(seguro);
 }
 
